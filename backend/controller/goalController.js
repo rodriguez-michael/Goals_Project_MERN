bd161f2a819af18dc1ebca94c5c366d470563de2
rodriguez-michael <github@michaelrodriguez.dev>
2022-03-10 21:34:24 -0600

    added express-async-handler error handling

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -1,37 +1,47 @@
+//functions are async - using mongoose to interact with mongoDB returns a promise
+//express async handler used instead of try/catch 
+const asyncHandler = require('express-async-handler')
+
 // @desc     Get all goals
 // @route    GET /api/goals
 // @access   Private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Get goals' })
-}
+})
 
 // @desc     Set goal
 // @route    POST /api/goals
 // @access   Private
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
+
+  if(!req.body.text){
+    res.status(400)
+    throw new Error('Please add a text field')
+  }
+
   res.status(200).json({message: "Post goal"})
-}
+})
 
 // @desc     Get a goal
 // @route    GET /api/goals/:id
 // @access   Private
-const getGoal = (req, res) => {
+const getGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Get goal ${req.params.id}` })
-}
+})
 
 // @desc     Update goal
 // @route    PUT /api/goals/:id
 // @access   Private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({message: `Put goal ${req.params.id}`})
-}
+})
 
 // @desc     Delete goal
 // @route    DELETE /api/goals/:id
 // @access   Private
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({message: `Delete goal ${req.params.id}`})
-}
+})
 
 module.exports = {
   getGoals,
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
 const port = process.env.PORT
 
 const app = express()
 
+//middleware to send body in post request
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+//goal routes
 app.use('/api/goals', require('./routes/goalRoutes'))
+
+//middleware to overite default express error handler
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
