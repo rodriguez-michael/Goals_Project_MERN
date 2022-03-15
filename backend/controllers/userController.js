c7a29e617f353e8e3a0d9fd9d15a8285ebae4e2d
rodriguez-michael <github@michaelrodriguez.dev>
2022-03-14 22:20:26 -0500

    able to register a user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,23 +1,69 @@
+//for authentication
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+
+//functions are async - using mongoose to interact with mongoDB returns a promise
+//express async handler used instead of try/catch 
+const asyncHandler = require('express-async-handler')
+
+//goal model
+const User = require('../models/userModel')
+
 // @desc     Register user
 // @route    POST /api/users
 // @access   Private
-const registerUser = (req, res) => {
-  res.json({ message: 'Register User' })
-}
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  if(!name || !email || !password) {
+    res.status(400)
+    throw new Error('Please add all fields')
+  }
+
+  //check if user exists
+  const userExists = await User.findOne({ email })
+
+  if(userExists){
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  //hash password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  //create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword
+  })
+
+  if(user){
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email
+    })
+  } else{
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
+})
 
 // @desc     Authenticate user
 // @route    POST /api/users/login
 // @access   Private
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
   res.json({ message: 'Login User' })
-}
+})
 
 // @desc     Get user data
 // @route    GET /api/users/me
 // @access   Private
-const getUser = (req, res) => {
+const getUser = asyncHandler(async (req, res) => {
   res.json({ message: 'User data displayed' })
-}
+})
 
 module.exports = {
   registerUser,
