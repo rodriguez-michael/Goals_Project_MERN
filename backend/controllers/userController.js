1b763dba1a7336d99d2fa70d8768305f60ad0751
rodriguez-michael <github@michaelrodriguez.dev>
2022-03-15 21:51:44 -0500

    able to match email and password against database to login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -55,7 +55,22 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route    POST /api/users/login
 // @access   Private
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: 'Login User' })
+
+  const { email, password } = req.body
+
+  //check if user email exists and then if password matches
+  const user = await User.findOne({ email })
+
+  if(user && (await bcrypt.compare(password, user.password))){
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email
+    })
+  } else{
+    res.status(400)
+    throw new Error('Invalid login credentials')    
+  }
 })
 
 // @desc     Get user data
