69fecf457e76e38d50b98c5f04ba68c00d97fe24
rodriguez-michael <github@michaelrodriguez.dev>
2022-03-18 21:32:02 -0500

    created authentication middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -79,7 +79,13 @@ const loginUser = asyncHandler(async (req, res) => {
 // @route    GET /api/users/me
 // @access   Private
 const getUser = asyncHandler(async (req, res) => {
-  res.json({ message: 'User data displayed' })
+  const {_id, name, email } = await User.findById(req.user.id)
+
+  res.status(200).json({
+    id: _id,
+    name,
+    email,
+  })
 })
 
 //Generate JWT
