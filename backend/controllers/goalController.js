cf3db83c90200e7d74448c9d64ba7655e046b039
rodriguez-michael <github@michaelrodriguez.dev>
2022-03-19 17:05:06 -0500

    able to update and delete goals based on user token

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -2,8 +2,9 @@
 //express async handler used instead of try/catch 
 const asyncHandler = require('express-async-handler')
 
-//goal model
+//models
 const Goal = require('../models/goalModel')
+const User = require("../models/userModel")
 
 // @desc     Get all goals
 // @route    GET /api/goals
@@ -37,6 +38,20 @@ const setGoal = asyncHandler(async (req, res) => {
 const getGoal = asyncHandler(async (req, res) => {
   const goal = await Goal.findById(req.params.id)
 
+  const user = await User.findById(req.user.id)
+
+  //check for user
+  if(!user){
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  //make sure the logged in user matches the goal user
+  if(goal.user.toString() !== user.id){
+    res.status(401)
+    throw new Error('User not authorized')
+  }
+
   if(!goal){
     res.status(400)
     throw new Error('Goal not found')
@@ -56,6 +71,20 @@ const updateGoal = asyncHandler(async (req, res) => {
     throw new Error('Goal not found')
   }
 
+  const user = await User.findById(req.user.id)
+
+  //check for user
+  if(!user){
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  //make sure the logged in user matches the goal user
+  if(goal.user.toString() !== user.id){
+    res.status(401)
+    throw new Error('User not authorized')
+  }
+
   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
   })
@@ -72,6 +101,20 @@ const deleteGoal = asyncHandler(async (req, res) => {
   if(!goal){
     res.status(400)
     throw new Error('Goal not found')
+  }
+
+  const user = await User.findById(req.user.id)
+
+  //check for user
+  if(!user){
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  //make sure the logged in user matches the goal user
+  if(goal.user.toString() !== user.id){
+    res.status(401)
+    throw new Error('User not authorized')
   }
 
   await goal.remove()
